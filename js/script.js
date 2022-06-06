3b11aba9b174c68c9c03b03d5f2db5214093205b
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-06 10:51:15 +0530

    added functionality

--- js/script.js
+++ js/script.js
@@ -2,8 +2,26 @@
 let iconClose = document.querySelector('.icon-close');
 let modal = document.querySelector('.modal')
 let overlay = document.querySelector('.overlay')
-
-iconClose.addEventListener('click',()=>{
+let Rules = document.querySelector('.rules');
+let StepOne = document.querySelector('.step-1');
+let Step2 = document.querySelector('.step-2')
+let UserSelection = "";
+iconClose.addEventListener('click', () => {
     modal.classList.add('hide-modal')
     overlay.classList.add('hide-overlay')
+})
+
+Rules.addEventListener('click',()=>{
+    modal.classList.remove('hide-modal')
+    overlay.classList.remove('hide-overlay')
+})
+StepOne.addEventListener('click', (e) => {
+    UserSelection = e.target.id;
+    setTimeout(() => {
+        StepOne.classList.add('remove');
+
+    }, 200)
+    setTimeout(()=>{
+        Step2.classList.remove('remove');
+    },200)
 })
